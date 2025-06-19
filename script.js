1f8d0e423d1e1e0e056787de94eac773647e9967
hannahliu123 <hannah.liu2010@gmail.com>
2025-06-19 19:25:43 -0400

    nav bar
    still needs styling tho
    got some js done

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+// ========== Widely Used Variables ==========
+const aboutMeSection = document.getElementById("about-me-section");
+
+// Show Nav Bar
+const navBar = document.getElementById("nav-bar");
+const aboutMeYPos = aboutMeSection.getBoundingClientRect().top;
 let prevScroll = 0;
 window.addEventListener("scroll", () => {
-    const arrow = document.getElementById("down-arrow");
     let currScroll = window.scrollY;
 
-    if (currScroll > prevScroll) {  // scrolling down
-        arrow.classList.add("active");
-    } else if (currScroll < prevScroll) {   // scrolling up
-        arrow.classList.remove("active");
+    if (currScroll+1 >= aboutMeYPos) {
+        navBar.classList.add("active");
+    } else {
+        navBar.classList.remove("active");
+        const arrow = document.getElementById("down-arrow");
+        if (currScroll > prevScroll) {  // scrolling down
+            arrow.classList.add("active");
+            setTimeout(() => {
+                arrow.classList.remove("active");
+            }, 750);   // 750 ms
+        } else if (currScroll < prevScroll) {   // scrolling up
+            arrow.classList.remove("active");
+        }
     }
 
     prevScroll = currScroll;
+});
+
+const aboutMeBtn = document.getElementById("about-me-button");
+aboutMeBtn.addEventListener("click", () => {
+    aboutMeSection.scrollIntoView({behavior: "smooth"});
 });
