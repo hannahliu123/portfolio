0f902cb665df741f8f12d4a641f0615ecf688487
hannahliu123 <hannah.liu2010@gmail.com>
2025-06-22 19:38:37 -0400

    animate instead for scrolling
    added some details too
    hope it works on mobile...

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // ========== Widely Used Variables ==========
     const aboutMeSection = document.getElementById("about-me-section");
+    let direction = 1;
+    const filmstripContainer = document.getElementById("filmstrip");
 
     // ========== Functions ======================
+    function animateScroll() {
+        const endXScroll = filmstripContainer.scrollWidth - filmstripContainer.clientWidth;
+        const currXScroll = filmstripContainer.scrollLeft;
 
+        filmstripContainer.scrollLeft += direction*1;
+        if (direction === 1 && currXScroll >= endXScroll - 1) direction = -1;
+        else if (direction === -1 && currXScroll <= 1) direction = 1;
+
+        if (filmstripContainer.classList.contains("scroll")) requestAnimationFrame(animateScroll);
+    }
 
     // ========== Event Listeners ================
     const navBar = document.getElementById("nav-bar");
@@ -38,29 +49,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     // filmstrip scroll
-    let direction = 1;
-    const filmstripContainer = document.getElementById("filmstrip");
     filmstripContainer.addEventListener("click", () => {
         filmstripContainer.classList.toggle("scroll");
-        let scrollID = setInterval(() => {
-            const endXScroll = filmstripContainer.scrollWidth - filmstripContainer.clientWidth;
-            filmstripContainer.scrollBy(direction*5, 0);
-            currXScroll = filmstripContainer.scrollLeft;
-
-            if (!filmstripContainer.classList.contains("scroll")) clearInterval(scrollID);
-            else if (direction === 1 && currXScroll >= endXScroll) direction = -1;
-            else if (direction === -1 && currXScroll === 0) direction = 1;
-        }, 50);     // scroll right 5px this every 50 ms
-    });
-
-    const test1 = document.getElementById("test1");
-    const test2 = document.getElementById("test2");
-    const test3 = document.getElementById("test3");
-    const test4 = document.getElementById("test4");
-    filmstripContainer.addEventListener("scroll", () => {
-        test1.textContent = direction;
-        test2.textContent = currXScroll;
-        test3.textContent = filmstripContainer.scrollWidth - filmstripContainer.clientWidth;
-        test4.textContent = direction === 1;
+        if (filmstripContainer.classList.contains("scroll")) requestAnimationFrame(animateScroll);
     });
 });
