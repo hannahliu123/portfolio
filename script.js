0612f9dd22140b32246acaf11d5b382afad5ecfc
hannahliu123 <hannah.liu2010@gmail.com>
2025-06-21 21:32:20 -0400

    auto scrolling
    AHHH YESSSSSS IM SO PROUD OMG!!!! finally got the auto scrolling to go back and forth. kinda gave up on the continuous 1-direction scrolling looping through all the pictures but uh! that's ok! main problem rn is that it kinda doesn't look that good... i mean it doesn't really fit into the aesethetic. I'll probably change the color and maybe figure out some icons or other graphics to add to make it look better

--- script.js
+++ script.js
@@ -4,33 +4,52 @@ document.addEventListener("DOMContentLoaded", () => {
     // ========== Widely Used Variables ==========
     const aboutMeSection = document.getElementById("about-me-section");
 
-    // ========== Event Listeners ==========
+    // ========== Functions ======================
+
+
+    // ========== Event Listeners ================
     const navBar = document.getElementById("nav-bar");
     const aboutMeYPos = aboutMeSection.getBoundingClientRect().top;
     let prevScroll = 0;
     window.addEventListener("scroll", () => {
-        let currScroll = window.scrollY;
+        let currYScroll = window.scrollY;
 
-        if (currScroll+1 >= aboutMeYPos) {
+        if (currYScroll+1 >= aboutMeYPos) {
             navBar.classList.add("active");
         } else {
             navBar.classList.remove("active");
             const arrow = document.getElementById("down-arrow");
-            if (currScroll > prevScroll) {  // scrolling down
+            if (currYScroll > prevScroll) {  // scrolling down
                 arrow.classList.add("active");
                 setTimeout(() => {
                     arrow.classList.remove("active");
                 }, 750);   // 750 ms
-            } else if (currScroll < prevScroll) {   // scrolling up
+            } else if (currYScroll < prevScroll) {   // scrolling up
                 arrow.classList.remove("active");
             }
         }
 
-        prevScroll = currScroll;
+        prevScroll = currYScroll;
     });
 
     const aboutMeBtn = document.getElementById("about-me-button");
     aboutMeBtn.addEventListener("click", () => {
         aboutMeSection.scrollIntoView({behavior: "smooth"});
+    });
+
+    // filmstrip scroll
+    let direction = 1;
+    const filmstripContainer = document.getElementById("filmstrip");
+    filmstripContainer.addEventListener("click", () => {
+        filmstripContainer.classList.toggle("scroll");
+        let scrollID = setInterval(() => {
+            filmstripContainer.scrollBy({left: direction*5, top: 0, behavior: "smooth"});
+            currXScroll = filmstripContainer.scrollLeft;
+            const endXScroll = filmstripContainer.scrollWidth - filmstripContainer.clientWidth;
+
+            if (!filmstripContainer.classList.contains("scroll")) clearInterval(scrollID);
+            else if (direction === 1 && currXScroll >= endXScroll) direction = -1;
+            else if (direction === -1 && currXScroll === 0) direction = 1;
+        }, 50);     // scroll right 5px this every 50 ms
     });
 });
