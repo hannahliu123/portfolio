116b0182855c0ce69d873bd2d074048339fc073f
Hannah Liu <hannah.liu2010@gmail.com>
2026-09-23 09:01:59 -0400

    fencing :))

---
+++ js/firebase/fencing-updates.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCs0tT5ACiOgNBkpmbgpt_9d92lfGexy0M",
+    authDomain: "my-blog-986f0.firebaseapp.com",
+    projectId: "my-blog-986f0",
+    storageBucket: "my-blog-986f0.firebasestorage.app",
+    messagingSenderId: "220461177065",
+    appId: "1:220461177065:web:8be8ecf1dcb07cef657878",
+    measurementId: "G-6Y79WDNFCP"
+};
+
+// Initialize Firebase & Firestore db
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);       // my connected Firestore database
+
+const aboutMeDocRef = doc(db, "updates", "about-me-updates");    // reference to desired document
+const aboutMeDocSnapshop = await getDoc(aboutMeDocRef);   // snapshot of current data
+
+if (!aboutMeDocSnapshop.exists()) {
+    console.log("No document found.");
+} else {
+    const aboutMeUpdates = aboutMeDocSnapshop.data();
+    const updates = document.getElementById("updates");
+    updates.innerHTML = aboutMeUpdates.fencing;
+}
